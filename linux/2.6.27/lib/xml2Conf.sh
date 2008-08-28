949a7b040fdc42d239ecb5c74b0f45d7f574e7f1
Thomas Ritz <tritz@infopark.de>
2008-08-28 14:07:44 +0200

    init

---
+++ linux/2.6.27/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/local/Develop/tilo/trunk/External-osdep/libxml2/linux/2.6.27/lib"
+XML2_LIBS="-lxml2 -lz   -lm "
+XML2_INCLUDEDIR="-I/local/Develop/tilo/trunk/External-osdep/libxml2/linux/2.6.27/include/libxml2"
+MODULE_VERSION="xml2-2.6.27"
+
