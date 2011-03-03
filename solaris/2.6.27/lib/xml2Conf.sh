5ee06f48274a94dce6b736ac075c68be51e073cc
Tilo Prütz <tilo@infopark.de>
2011-03-03 09:46:54 +0100

    2.7.8 win&lin shared

--- solaris/2.6.27/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/local/Develop/develop/trunk/External-osdep/libxml2/solaris/2.6.27/lib -R/local/Develop/develop/trunk/External-osdep/libxml2/solaris/2.6.27/lib"
-XML2_LIBS="-lxml2 -lz -lpthread  -liconv -lm -lsocket -lnsl "
-XML2_INCLUDEDIR="-I/local/Develop/develop/trunk/External-osdep/libxml2/solaris/2.6.27/include/libxml2"
-MODULE_VERSION="xml2-2.6.27"
-
---
+++ windows/2.7.8/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/tilo/work/fiona/merge/External/libxml2/windows/2.7.8/lib"
+XML2_LIBS="-lxml2     "
+XML2_INCLUDEDIR="-I/home/tilo/work/fiona/merge/External/libxml2/windows/2.7.8/include/libxml2"
+MODULE_VERSION="xml2-2.7.8"
+
