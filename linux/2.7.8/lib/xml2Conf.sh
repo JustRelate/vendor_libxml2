459ef4496444ffb83d0fad14db93dd6c1beaf14a
Tilo Prütz <tilo@infopark.de>
2013-09-20 22:07:03 +0200

    [2.7.8] nur noch Linux

---
+++ linux/2.7.8/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/tmp/libxml2-2.7.8/lib"
+XML2_LIBS="-lxml2 -L/home/vagrant/infopark-cms/External/zlib/linux/1.2.3/lib -lz   -lm "
+XML2_INCLUDEDIR="-I/tmp/libxml2-2.7.8/include/libxml2"
+MODULE_VERSION="xml2-2.7.8"
+
